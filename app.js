7855341b1ebca00e1eb69123ab64681380c86166
choimoonjong <162957454+choimoonjong@users.noreply.github.com>
2024-11-01 00:15:16 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+//require('./app_server/models/db')
+require('./app_api/models/db');
+
+// var indexPouter = require('./routes/index');
+const indexRouter = require('./app_server/routes/index'); //원래 앞에 var indexRouter 이었음
+const apiRouter = require('./app_api/routes/index'); //원래 앞에 var usersRouter 이었음
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server','views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
